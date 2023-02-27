7616b0f9ac846433484a86d7057c8e7ade4e79a6
Lelik Jan <21alex7777@gmail.com>
2023-02-27 17:54:38 +0300

    feat: make solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,21 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+    }
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return (this.candidate = Math.round((this.max + this.min) / 2));
     }
 
     lower() {
-
+        this.max = this.candidate ;
     }
 
     greater() {
-
+        this.min = this.candidate ;
     }
 }
 
